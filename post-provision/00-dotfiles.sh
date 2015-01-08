2ece7ec2b2b72b71d7b9e47fe0a54b65fa779c60
Dane MacMillan <work@danemacmillan.com>
2015-01-07 20:16:41 -0500

    Removed newlines as Windows was interpreting them as ^M.

--- post-provision/00-dotfiles.sh
+++ post-provision/00-dotfiles.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # Install dotfiles
 echo -e "\nInstalling dotfiles from https://github.com/danemacmillan/dotfiles. This will take a minute."
 echo -e "--------------------------------------------------------------------------------"
@@ -7,9 +6,7 @@ echo -e "Installing for 'root' user..."
 cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh &> /dev/null
 echo -e "Installing for '$USER_USER' user..."
 su $USER_USER -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh &> /dev/null"
-
 # Set permissions on regular user.
 echo -e "Setting permissions for $USER_USER:$USER_GROUP on /home/$USER_USER"
 chown -R $USER_USER:$USER_GROUP /home/$USER_USER
-
 echo -e "\ndotfiles installed successfully!"
