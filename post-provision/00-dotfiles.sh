005de5d58846cc1905e99cb0131f5a718e4af96a
Dane MacMillan <work@danemacmillan.com>
2015-01-12 12:24:06 -0500

    Try to resolve /dev/null error.

--- post-provision/00-dotfiles.sh
+++ post-provision/00-dotfiles.sh
@@ -2,18 +2,15 @@
 # Install dotfiles
 # Redirect stdout to /dev/null because vim vundler runs some semi-interactive
 # installer, which screws with terminal buffer.
+# There are bizarre /dev/null permission errors when redirecting to it with su -c
 echo -e "\nInstalling dotfiles from https://github.com/danemacmillan/dotfiles. This will take a minute."
 echo -e "--------------------------------------------------------------------------------"
 echo -e "Installing for '$USER_USER' user..."
-# http://fixunix.com/suse/255347-bash-dev-null-permission-denied-problem-keeps-happening.html
-rm -f /dev/null
-mknod -m 0666 /dev/null c 1 3
-su $USER_USER -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh 1> /dev/null"
+su $USER_USER -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh"
 # Set permissions on regular user.
-echo -e "Setting permissions for $USER_USER:$USER_GROUP on /home/$USER_USER"
-chown -R $USER_USER:$USER_GROUP /home/$USER_USER
+#echo -e "Setting permissions for $USER_USER:$USER_GROUP on /home/$USER_USER"
+#chown -R $USER_USER:$USER_GROUP /home/$USER_USER
 echo -e "Installing for 'root' user..."
-rm -f /dev/null
-mknod -m 0666 /dev/null c 1 3
-cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh 1> /dev/null
+#cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh 1> /dev/null
+cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh
 echo -e "\ndotfiles installed successfully!"
