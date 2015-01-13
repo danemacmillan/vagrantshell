b469b0fb8ba3b87cd82b9cbe769d6802e3758c2f
Dane MacMillan <work@danemacmillan.com>
2015-01-12 22:46:28 -0500

    Adjust dotfiles post-provision script.

--- post-provision/00-dotfiles.sh
+++ post-provision/00-dotfiles.sh
@@ -6,11 +6,12 @@
 echo -e "\nInstalling dotfiles from https://github.com/danemacmillan/dotfiles. This will take a minute."
 echo -e "--------------------------------------------------------------------------------"
 echo -e "Installing for '$USER_USER' user..."
-sudo -u $USER_USER -H -s bash -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh"
+#sudo -u $USER_USER -H -s bash -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh"
+sudo -u $USER_USER -H -i bash -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh"
 # Set permissions on regular user.
 #echo -e "Setting permissions for $USER_USER:$USER_GROUP on /home/$USER_USER"
 #chown -R $USER_USER:$USER_GROUP /home/$USER_USER
 echo -e "Installing for 'root' user..."
-#cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh 1> /dev/null
+#cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh &> /dev/null
 cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && cd .dotfiles && source bootstrap.sh
 echo -e "\ndotfiles installed successfully!"
