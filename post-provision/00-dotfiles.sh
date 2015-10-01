71244c95887164fe76e15d4731d15fa3eefcb2e0
Dane MacMillan <work@danemacmillan.com>
2015-10-01 13:52:39 -0400

    Update post-provision dotfiles script.

--- post-provision/00-dotfiles.sh
+++ post-provision/00-dotfiles.sh
@@ -5,6 +5,6 @@ echo -e "-----------------------------------------------------------------------
 echo -e "Installing for '$USER_USER' user..."
 sudo -u $USER_USER -H -i bash -c "cd /home/$USER_USER && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && source /home/$USER_USER/.dotfiles/bootstrap.sh"
 echo -e "Installing for 'root' user..."
-cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && source /root/.dotfiles/bootstrap.sh
+cd /root && git clone git@github.com:danemacmillan/dotfiles.git .dotfiles && source /root/.dotfiles/dotfiles
 echo -e "\ndotfiles installed successfully (unless you are on Windows)!"
 echo -e "Windows users must 'vagrant ssh' in and run 'source ~/.dotfiles/bootstrap.sh' the first time."
